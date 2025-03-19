380cf565cc0934178f002b7e75100f65d0dbdbeb
DevChawla <119198647+DevChawla@users.noreply.github.com>
2025-03-19 16:12:33 +0530

    redux counter app

---
+++ src/app/components/Counter.tsx
@@ -0,0 +1,27 @@
+"use client"; // Required for client-side interactivity
+
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "@/redux/store";
+import { increment, decrement, incrementByAmount,decrementByAmount } from "@/redux/slices/counterSlice";
+import "./Counter.css"; 
+
+export default function Counter() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+
+  return (
+    <div className="counter-container">
+    <h1>Counter: {count}</h1>
+    <div className="button-group">
+      <button onClick={() => dispatch(increment())}>+ 1</button>
+      <button onClick={() => dispatch(decrement())} disabled={count === 0}>
+        - 1
+      </button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>+ 5</button>
+      <button onClick={() => dispatch(decrementByAmount(5))} disabled={count < 5}>
+        - 5
+      </button>
+    </div>
+  </div>
+  );
+}
