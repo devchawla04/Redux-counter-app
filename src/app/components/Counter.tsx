113eaf9e0b8b6fe3364aef9fd709966955f815fe
DevChawla <119198647+DevChawla@users.noreply.github.com>
2025-03-19 16:19:05 +0530

    test deployment

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -3,14 +3,16 @@
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "@/redux/store";
 import { increment, decrement, incrementByAmount,decrementByAmount } from "@/redux/slices/counterSlice";
-import "./Counter.css"; 
+import "./counter.css"; 
 
 export default function Counter() {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch<AppDispatch>();
 
   return (
+    
     <div className="counter-container">
+        <h1>Made By Dev Chawla</h1>
     <h1>Counter: {count}</h1>
     <div className="button-group">
       <button onClick={() => dispatch(increment())}>+ 1</button>
